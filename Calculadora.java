359b5730dcd69ffb3005b29e5027a9c119ddb9f4
AndresVargas0 <1458586@senati.pe>
2023-08-17 18:16:09 -0500

    Calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -1,24 +1,22 @@
+package com.mycompany.senati;
+//Importar Scanner
 import java.util.Scanner;
-
+//Crear la clase publica Calculadora
 public class Calculadora {
+    //Crear el Metodo Main para recibir los Argumentos
     public static void main(String[] args) {
+        //Inicializar Scanner
         Scanner sc = new Scanner(System.in);
-        int N1;
-        int N2;
-        
-        System.out.println("Primer Numero: ");
-        N1 = sc.nextInt();
-        
-        System.out.println("Segundo Numero: ");
-        N2 = sc.nextInt();
-        
-        int suma = N1 + N2;
-        int resta = N1 - N2;
-        int division = N1 / N2;
-        int multiplicacion = N1 * N2;
-        
-       System.out.println("La suma es: "+suma+
-               "\nLa resta es: "+resta+
+        //Creacion de Variables para los Numeros Ingresados
+        float N1, N2;
+        //Mostrar e Ingresar los Datos desde la Consola
+        System.out.println("Primer Numero: "); N1 = sc.nextInt();
+        System.out.println("Segundo Numero: "); N2 = sc.nextInt();
+        //Operaciones
+        float suma = N1+N2, resta = N1-N2, division = N1/N2, multiplicacion = N1*N2;
+        //Imprimir Repuestas
+        System.out.println("La Suma es: "+suma+
+               "\nLa Resta es: "+resta+
                "\nLa Division es: "+division+
                "\nLa Multiplicacion es: "+multiplicacion);
     }
