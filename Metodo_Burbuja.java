3122c139fd72731bcb89b38ab193ce9933b7beee
AndresVargas0 <1458586@senati.pe>
2023-08-17 19:09:42 -0500

    RecoleccionNumeros

--- Metodo_Burbuja.java
+++ Metodo_Burbuja.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 
 public class Metodo_Burbuja {
     public static void main(String arg[]){
-        
+        //Inicializar Scanner
+        Scanner sc = new Scanner(System.in);
+            //Crear las Variables que Alojen los Numeros a Ordenar
+            int n1, n2, n3, n4, n5;
+            //Recoger la Informacion desde la Consola
+            System.out.println("Primer Numero: "); n1 = sc.nextInt();
+            System.out.println("Segundo Numero: "); n2 = sc.nextInt();
+            System.out.println("Terver Numero: "); n3 = sc.nextInt();
+            System.out.println("Cuarto Numero: "); n4 = sc.nextInt();
+            System.out.println("Quinto Numero: "); n5 = sc.nextInt();
         }
 }
