55ac6ce9eea512fda3ee1bb0e7218143c58d994d
sandpinedo <1440690@senati.pe>
2023-08-31 16:20:14 -0500

    Projects

---
+++ C#/Calculadora/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button7 = new Button();
+            button1 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlLightLight;
+            button2.Cursor = Cursors.Hand;
+            button2.Location = new Point(28, 113);
+            button2.Name = "button2";
+            button2.Size = new Size(72, 39);
+            button2.TabIndex = 3;
+            button2.Text = "6";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlLightLight;
+            button3.Cursor = Cursors.Hand;
+            button3.Location = new Point(28, 160);
+            button3.Name = "button3";
+            button3.Size = new Size(72, 39);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button1_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ControlLightLight;
+            button4.Cursor = Cursors.Hand;
+            button4.Location = new Point(28, 68);
+            button4.Name = "button4";
+            button4.Size = new Size(72, 39);
+            button4.TabIndex = 3;
+            button4.Text = "9";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button1_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ControlLightLight;
+            button7.Cursor = Cursors.Hand;
+            button7.Location = new Point(262, 68);
+            button7.Name = "button7";
+            button7.Size = new Size(72, 39);
+            button7.TabIndex = 3;
+            button7.Text = "-";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlLightLight;
+            button1.Cursor = Cursors.Hand;
+            button1.Location = new Point(106, 160);
+            button1.Name = "button1";
+            button1.Size = new Size(72, 39);
+            button1.TabIndex = 3;
+            button1.Text = "2";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ControlLightLight;
+            button5.Cursor = Cursors.Hand;
+            button5.Location = new Point(183, 160);
+            button5.Name = "button5";
+            button5.Size = new Size(72, 39);
+            button5.TabIndex = 3;
+            button5.Text = "1";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ControlLightLight;
+            button6.Cursor = Cursors.Hand;
+            button6.Location = new Point(106, 113);
+            button6.Name = "button6";
+            button6.Size = new Size(72, 39);
+            button6.TabIndex = 3;
+            button6.Text = "5";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button1_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ControlLightLight;
+            button8.Cursor = Cursors.Hand;
+            button8.Location = new Point(106, 68);
+            button8.Name = "button8";
+            button8.Size = new Size(72, 39);
+            button8.TabIndex = 3;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button1_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ControlLightLight;
+            button9.Cursor = Cursors.Hand;
+            button9.Location = new Point(184, 68);
+            button9.Name = "button9";
+            button9.Size = new Size(72, 39);
+            button9.TabIndex = 3;
+            button9.Text = "7";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button1_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ControlLightLight;
+            button10.Cursor = Cursors.Hand;
+            button10.Location = new Point(184, 113);
+            button10.Name = "button10";
+            button10.Size = new Size(72, 39);
+            button10.TabIndex = 3;
+            button10.Text = "4";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button1_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = SystemColors.ControlLightLight;
+            button11.Cursor = Cursors.Hand;
+            button11.Location = new Point(261, 160);
+            button11.Name = "button11";
+            button11.Size = new Size(72, 39);
+            button11.TabIndex = 3;
+            button11.Text = "x";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button1_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = SystemColors.ControlLightLight;
+            button12.Cursor = Cursors.Hand;
+            button12.Location = new Point(262, 113);
+            button12.Name = "button12";
+            button12.Size = new Size(72, 39);
+            button12.TabIndex = 3;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button1_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = SystemColors.ControlLightLight;
+            button13.Cursor = Cursors.Hand;
+            button13.Location = new Point(106, 206);
+            button13.Name = "button13";
+            button13.Size = new Size(72, 39);
+            button13.TabIndex = 3;
+            button13.Text = "0";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button1_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = SystemColors.ControlLightLight;
+            button14.Cursor = Cursors.Hand;
+            button14.Location = new Point(260, 206);
+            button14.Name = "button14";
+            button14.Size = new Size(72, 39);
+            button14.TabIndex = 3;
+            button14.Text = "/";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button1_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = SystemColors.ControlLightLight;
+            button15.Cursor = Cursors.Hand;
+            button15.Location = new Point(182, 206);
+            button15.Name = "button15";
+            button15.Size = new Size(72, 39);
+            button15.TabIndex = 3;
+            button15.Text = ".";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button1_Click;
+            // 
+            // button16
+            // 
+            button16.BackColor = SystemColors.ControlLightLight;
+            button16.Cursor = Cursors.Hand;
+            button16.Location = new Point(28, 206);
+            button16.Name = "button16";
+            button16.Size = new Size(72, 39);
+            button16.TabIndex = 3;
+            button16.Text = "CE";
+            button16.UseVisualStyleBackColor = false;
+            button16.Click += button1_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.ControlLightLight;
+            button17.Cursor = Cursors.Hand;
+            button17.Location = new Point(106, 252);
+            button17.Name = "button17";
+            button17.Size = new Size(72, 39);
+            button17.TabIndex = 3;
+            button17.Text = "=";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(28, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(306, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(363, 312);
+            Controls.Add(textBox1);
+            Controls.Add(button8);
+            Controls.Add(button4);
+            Controls.Add(button6);
+            Controls.Add(button2);
+            Controls.Add(button12);
+            Controls.Add(button10);
+            Controls.Add(button16);
+            Controls.Add(button13);
+            Controls.Add(button15);
+            Controls.Add(button17);
+            Controls.Add(button14);
+            Controls.Add(button11);
+            Controls.Add(button9);
+            Controls.Add(button5);
+            Controls.Add(button7);
+            Controls.Add(button1);
+            Controls.Add(button3);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button button8;
+        private Button button4;
+        private Button button6;
+        private Button button2;
+        private Button button10;
+        private Button button9;
+        private Button button5;
+        private Button button7;
+        private Button button1;
+        private Button button3;
+        private Button button12;
+        private Button button11;
+        private Button button16;
+        private Button button13;
+        private Button button15;
+        private Button button14;
+        private Button button17;
+        private TextBox textBox1;
+    }
+}
---
+++ C#/Calculadora/Form1.cs
@@ -0,0 +1,30 @@
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
